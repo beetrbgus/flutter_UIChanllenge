4ea59a371e350cab3160e9806df3225c204d1f3e
beetrbgus <devbeetrb0214@gmail.com>
2023-01-06 05:30:37 +0900

    ( 추가

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold
+      home: Scaffold(
         backgroundColor: Color(0xFF181818),
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
