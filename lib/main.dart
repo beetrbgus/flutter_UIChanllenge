b0b23cb9dc3606085d2c09167d7b59433d3412dc
beetrbgus <devbeetrb0214@gmail.com>
2023-01-06 12:52:05 +0900

    Custom Widget 생성해 컴포넌트화

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
 
 void main() {
   runApp(App());
@@ -66,22 +67,16 @@ class App extends StatelessWidget {
                 height: 10,
               ),
               Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: const Color(0xffF2B33A),
-                        borderRadius: BorderRadius.circular(35)),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                    ),
-                  )
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(
+                      text: 'Transfer',
+                      bgColor: Color(0xffF2B33A),
+                      textColor: Colors.black),
+                  Button(
+                      text: 'Request',
+                      bgColor: Color(0xff1F2123),
+                      textColor: Colors.white),
                 ],
               ),
             ],
